ff4cb73cb610207db72e510da973b77cdaf450cf
Lostgrammer <carlos_cmvp@hotmail.com>
2024-07-13 16:34:22 -0600

    set TopicData record as data transfer object

---
+++ src/main/java/com/carlosvega/foro_hub/api/models/TopicData.java
@@ -0,0 +1,10 @@
+package com.carlosvega.foro_hub.api.models;
+
+public record TopicData(
+//        Integer id_usuario,
+        String id_usuario,
+        String message,
+        String nombreCurso,
+        String titulo
+) {
+}
