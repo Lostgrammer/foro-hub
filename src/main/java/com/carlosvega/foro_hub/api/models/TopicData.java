48f1990b258a70149f70b654258a30341c904139
Lostgrammer <carlos_cmvp@hotmail.com>
2024-07-14 08:49:16 -0600

    add elements to json and dto

--- src/main/java/com/carlosvega/foro_hub/api/models/TopicData.java
+++ src/main/java/com/carlosvega/foro_hub/api/models/TopicData.java
@@ -2,9 +2,17 @@ package com.carlosvega.foro_hub.api.models;
 
 public record TopicData(
 //        Integer id_usuario,
+//        String id_usuario,
+//        String message,
+//        String subjectName,
+//        String title,
+//        String creationDate,
+//        String author
         String id_usuario,
-        String message,
+        String mensaje,
         String nombreCurso,
-        String titulo
+        String titulo,
+        String fecha_creacion,
+        String autor
 ) {
 }
