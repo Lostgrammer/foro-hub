ec3e76500efbbb939cfb9e5e500994b6dc641041
Lostgrammer <carlos_cmvp@hotmail.com>
2024-07-16 00:02:27 -0600

    add ResponseEntity to put and delete http request

--- src/main/java/com/carlosvega/foro_hub/api/topic/TopicController.java
+++ src/main/java/com/carlosvega/foro_hub/api/topic/TopicController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -26,15 +27,22 @@ public class TopicController {
 
     @PutMapping
     @Transactional
-    public void updateTopic(@RequestBody @Valid UpdateTopicData updateTopicData){
+    public ResponseEntity updateTopic(@RequestBody @Valid UpdateTopicData updateTopicData){
         Topic topic = topicRepository.getReferenceById(updateTopicData.id_usuario());
         topic.updateData(updateTopicData);
+        return ResponseEntity.ok(new TopicResponseData(
+                topic.getUserId(),
+                topic.getMensaje(),
+                topic.getNombreCurso(),
+                topic.getTitulo(),
+                topic.getAutor()));
     }
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void deteleTopic(@PathVariable Long id){
+    public ResponseEntity deteleTopic(@PathVariable Long id){
         Topic topic = topicRepository.getReferenceById(id);
         topicRepository.delete(topic);
+        return ResponseEntity.noContent().build();
     }
 }
