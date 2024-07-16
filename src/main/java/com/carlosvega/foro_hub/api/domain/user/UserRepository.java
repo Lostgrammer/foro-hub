aefa94d322bc6f1e1e08e0ac689101067e007c4d
Lostgrammer <carlos_cmvp@hotmail.com>
2024-07-16 01:01:59 -0600

    add security authentication service and configuration to change to stateless

--- src/main/java/com/carlosvega/foro_hub/api/domain/user/UserRepository.java
+++ src/main/java/com/carlosvega/foro_hub/api/domain/user/UserRepository.java
@@ -1,4 +1,8 @@
 package com.carlosvega.foro_hub.api.domain.user;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    UserDetails findByLogin(String username);
 }
